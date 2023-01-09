82fa46ca25f86c7bdbba0f56b2cb2f8e6158dfb3
Manu Mohan <mail2manu04@gmail.com>
2023-01-09 12:55:30 +0000

    Initial Commit

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "project" {}
+variable "environment" {}
+variable "vpc_cidr" {}
+
+locals {
+
+  subnets = length(data.aws_availability_zones.available.names)
+
+}
+
+variable "enable_nat_gateway" {
+ 
+  type = bool
+  default = true
+
+}
